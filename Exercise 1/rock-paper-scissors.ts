faa01e7bbc5331a95e654e1b0ee034d3d24166e4
Joao Textor <joaotextor@gmail.com>
2023-03-12 08:51:44 -0300

    refactoring. replace if statements with lookup list

--- Exercise 1/rock-paper-scissors.ts
+++ Exercise 1/rock-paper-scissors.ts
@@ -1,7 +1,7 @@
 export enum Move {
-    Paper,
-    Rock,
-    Scissors,
+    Paper = "paper",
+    Rock = "rock",
+    Scissors = "scissors",
 }
 
 export enum Outcome {
@@ -15,41 +15,56 @@ interface IRockPaperScissors {
 }
 
 export function rockPaperScissors(): IRockPaperScissors {
+    //~ After refactoring
+    const scenarios = [
+        {playerMove: Move.Paper, opponentMove: Move.Rock, outcome: Outcome.PlayerWins},
+        {playerMove: Move.Paper, opponentMove: Move.Scissors, outcome: Outcome.PlayerLoses},
+        {playerMove: Move.Rock, opponentMove: Move.Paper, outcome: Outcome.PlayerLoses},
+        {playerMove: Move.Rock, opponentMove: Move.Scissors, outcome: Outcome.PlayerWins},
+        {playerMove: Move.Scissors, opponentMove: Move.Paper, outcome: Outcome.PlayerWins},
+        {playerMove: Move.Scissors, opponentMove: Move.Rock, outcome: Outcome.PlayerLoses},
+    ]
     return {
+        
         play(playerMove: Move, opponentMove: Move) {
-            let outcome: Outcome
 
-            if (playerMove === opponentMove) {
-                outcome = Outcome.Tie
-                return outcome
-            }
+            //~ After refactoring
+            const result = scenarios.find(scenario => scenario.playerMove === playerMove && scenario.opponentMove === opponentMove)
 
-            if (opponentMove === Move.Rock) {
-                outcome =
-                    playerMove === Move.Scissors
-                        ? Outcome.PlayerLoses
-                        : Outcome.PlayerWins
-                return outcome
-            }
+            return (result) ? result.outcome : Outcome.Tie
 
-            if (opponentMove === Move.Paper) {
-                outcome =
-                    playerMove === Move.Rock
-                        ? Outcome.PlayerLoses
-                        : Outcome.PlayerWins
-                return outcome
-            }
+            //~ Before refactoring
+            // let outcome: Outcome
+            // if (playerMove === opponentMove) {
+            //     outcome = Outcome.Tie
+            //     return outcome
+            // }
 
-            if (opponentMove === Move.Scissors) {
-                outcome =
-                    playerMove === Move.Paper
-                        ? Outcome.PlayerLoses
-                        : Outcome.PlayerWins
-                return outcome
-            }
+            // if (opponentMove === Move.Rock) {
+            //     outcome =
+            //         playerMove === Move.Scissors
+            //             ? Outcome.PlayerLoses
+            //             : Outcome.PlayerWins
+            //     return outcome
+            // }
 
-            outcome = Outcome.PlayerLoses
-            return outcome
+            // if (opponentMove === Move.Paper) {
+            //     outcome =
+            //         playerMove === Move.Rock
+            //             ? Outcome.PlayerLoses
+            //             : Outcome.PlayerWins
+            //     return outcome
+            // }
+
+            // if (opponentMove === Move.Scissors) {
+            //     outcome =
+            //         playerMove === Move.Paper
+            //             ? Outcome.PlayerLoses
+            //             : Outcome.PlayerWins
+            //     return outcome
+            // }
+
+            
         },
     }
 }
